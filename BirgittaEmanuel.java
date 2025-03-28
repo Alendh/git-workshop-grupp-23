82e84d52a235467a8c50bdc57a499fe49437b20c
Bjornheimer <christoffer.bjornheimer@gmail.com>
2025-03-28 09:27:59 +0100

    skapade klass

---
+++ BirgittaEmanuel.java
@@ -0,0 +1,6 @@
+public class BirgittaEmanuel {
+    public BirgittaEmanuel(){
+        System.out.println("This is my class, there are many classes like it but this is mine. Without my class, I am nothing. " +
+                "Without me, my class is nothing");
+    }
+}
--- Main.java
+++ Main.java
@@ -3,7 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        BirgittaEmanuel plopp = new BirgittaEmanuel();
+        System.out.println();
+        System.out.println();
         System.out.println("write something: ");
         System.out.println("your wrote: " + scanner.nextLine());
+
     }
+
 }
